2cc0da9fb4bee5e51f5ee84f78ace48387f337a5
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2021-04-05 21:44:05 +0200

    using Sets instead of Lists

--- maedle-maven-plugin/src/it/simple-it/verify.groovy
+++ maedle-maven-plugin/src/it/simple-it/verify.groovy
@@ -1,9 +1,9 @@
 def jarFile = new File(basedir, 'target/simple-it-1.0-SNAPSHOT.jar')
 assert jarFile.exists()
 
-def expected = [ 'META-INF/MANIFEST.MF', 'transform/me/GreeterMojoRewritten.class', 'transform/me/GreeterMojo.class', 'transform/me/GreeterMojoGradlePluginExtension.class', 'transform/me/GreeterMojoGradlePlugin.class', 'META-INF/gradle-plugins/foo.properties', 'META-INF/maven/sample.plugin.it/simple-it/pom.xml', 'META-INF/maven/sample.plugin.it/simple-it/pom.properties' ]
+Set expected = [ 'META-INF/MANIFEST.MF', 'transform/me/GreeterMojoRewritten.class', 'transform/me/GreeterMojo.class', 'transform/me/GreeterMojoGradlePluginExtension.class', 'transform/me/GreeterMojoGradlePlugin.class', 'META-INF/gradle-plugins/foo.properties', 'META-INF/maven/sample.plugin.it/simple-it/pom.xml', 'META-INF/maven/sample.plugin.it/simple-it/pom.properties' ]
+Set names = new java.util.zip.ZipFile(jarFile).entries().findAll { !it.directory }.collect { it.name }
 
-def names = new java.util.zip.ZipFile(jarFile).entries().findAll { !it.directory }.collect { it.name }
 assert expected.sort().equals(names.sort())
 
 
